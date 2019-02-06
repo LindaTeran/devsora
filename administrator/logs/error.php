f16e0456a362409d4ef3fb254b7cae77896d0888
linda <1630098@upv.edu.mx>
2019-02-06 00:35:42 +0000

    first commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-02-04 07:16:42 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-02-04T07:16:42+00:00	INFO 189.218.237.80	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-02-04T07:18:18+00:00	INFO 189.218.237.80	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-02-04T07:35:03+00:00	INFO 189.218.237.80	joomlafailure	Username and password do not match or you do not have an account yet.
+2019-02-04T07:45:46+00:00	INFO 189.218.237.80	joomlafailure	Username and password do not match or you do not have an account yet.
